f73a8e864b112fa6fc705ef34e5b3b0611913aab
Pablo Muñoz <pablomunoz@pablomunoz.dev>
2026-02-20 15:17:22 +0100

    add render markdown nvim plugin

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,48 +1,49 @@
 -- lazy.nvim plugin manager bootstrap and plugin loader
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-require('lazy').setup({
-  require 'plugins.editor',
-  require 'plugins.ui',
-  require 'plugins.navigation',
-  require 'plugins.lsp',
-  require 'plugins.formatting',
-  require 'plugins.completion',
-  require 'plugins.git',
-  require 'plugins.indent',
-  require 'plugins.lint',
-  require 'plugins.custom.oil',
-  require 'plugins.custom.lazygit',
-  require 'plugins.custom.highlight-colors',
+require("lazy").setup({
+	require("plugins.editor"),
+	require("plugins.ui"),
+	require("plugins.navigation"),
+	require("plugins.lsp"),
+	require("plugins.formatting"),
+	require("plugins.completion"),
+	require("plugins.git"),
+	require("plugins.indent"),
+	require("plugins.lint"),
+	require("plugins.custom.oil"),
+	require("plugins.custom.lazygit"),
+	require("plugins.custom.highlight-colors"),
+	require("plugins.custom.render-markdown"),
 }, {
-  lockfile = vim.fn.stdpath 'data' .. '/lazy-lock.json',
-  ui = {
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
+	lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
+	ui = {
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
 })
